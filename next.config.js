007205ac3a3dd1417c3db32347d1541194ac4cf9
Uppula Sai Kiran Reddy <32011704+saisk73@users.noreply.github.com>
2022-06-24 02:52:56 -0400

    Home page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  plugins: [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
+};
